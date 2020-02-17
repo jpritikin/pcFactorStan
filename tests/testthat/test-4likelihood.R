331695d691a00c58bf06b5408f3d6eba0b2f74bb
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-02-17 08:59:59 -0500

    Update test

--- tests/testthat/test-4likelihood.R
+++ tests/testthat/test-4likelihood.R
@@ -54,7 +54,7 @@ test_that("factor", {
   dl <- prepSingleFactorModel(dl, .2)
   m2 <- findModel("factor1_ll")
   f2 <- sampling(m2, dl, chains=1, cores=0, iter=1, seed=1,warmup=0, refresh=0)
-  expect_equal(get_logposterior(f2)[[1]], -108935.665, tolerance=1e-2, scale=1)
+  expect_equal(get_logposterior(f2)[[1]], -108931.7, tolerance=1e-1, scale=1)
   #cat(deparse(round(fivenum(extract(f2)$log_lik[1,]), 3)))
   expect_equal(fivenum(extract(f2)$log_lik[1,]),
                c(-54.219, -9.448, -4.866, -2.03, 0), tolerance=1e-2, scale=1)
